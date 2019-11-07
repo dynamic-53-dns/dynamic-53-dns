97229789a942745f26b8af6c89c46d493a29e6db
zhengdexing <570158976@qq.com>
2019-11-07 22:36:45 +0800

    新增定时器工具和定时器单元测试文件
    + 新增 ticker_test.go 文件
    * ticker.go 新增定时方法

--- util/ticker.go
+++ util/ticker.go
@@ -1,1 +1,30 @@
 package util
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// 互斥锁
+var wg sync.WaitGroup
+
+// 启动定时器
+func Star() {
+	// 设置定时器时间
+	ticker := time.NewTicker(time.Second)
+	wg.Add(1)
+	go func() {
+		var i int
+		for do := range ticker.C {
+			fmt.Println("sssssss====", i, "---", do)
+			if i == 5 {
+				wg.Done()
+				ticker.Stop()
+			}
+			i++
+		}
+	}()
+	// 阻塞主线程
+	wg.Wait()
+}
---
+++ util/ticker_test.go
@@ -0,0 +1,9 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestStar(t *testing.T) {
+	Star()
+}
